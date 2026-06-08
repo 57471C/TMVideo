dd8388625e2940f1a9270f1cd9040c603c3ea3a3
57471C <terryminett@gmail.com>
2026-06-08 16:57:48 +1000

    Preparing for uprev

--- ui/state.js
+++ ui/state.js
@@ -48,7 +48,7 @@ let volumeLevel = 1;
 // biome-ignore lint/style/useConst: Global state modified in other scripts
 let groupingMode = "lean";
 
-const APP_VERSION = "0.3.1";
+const APP_VERSION = "0.4.0";
 
 // biome-ignore lint/style/useConst: Global state modified in other scripts
 let isDrawing = false;
